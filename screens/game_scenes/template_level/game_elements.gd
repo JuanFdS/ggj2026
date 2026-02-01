03c3513fb7b4aeb5071d23872e9ccce0ea6ee23e
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2026-02-01 05:19:47 -0300

    Arregladas plataformas

--- screens/game_scenes/template_level/game_elements.gd
+++ screens/game_scenes/template_level/game_elements.gd
@@ -14,10 +14,11 @@ func _draw() -> void:
 			if not sprites_2d.is_empty():
 				var sprite_2d: Sprite2D = sprites_2d.front()
 				var modulate_color = Color(0.5,0.5,0.5,0.2)
+				var local_sprite_position = other().to_local(sprite_2d.global_position)
 				if sprite_2d.centered:
-					draw_texture(sprite_2d.texture, child.position - sprite_2d.texture.get_size() / 2.0, modulate_color)
+					draw_texture(sprite_2d.texture, local_sprite_position - sprite_2d.texture.get_size() / 2.0, modulate_color)
 				else:
-					draw_texture(sprite_2d.texture, child.position, modulate_color)
+					draw_texture(sprite_2d.texture, local_sprite_position, modulate_color)
 			else:
 				draw_circle(child.position, 5.0, Color.YELLOW)
 
