a8a314a715b2b96ecb8a00b35421419fdd1e7c53
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2026-02-01 05:40:47 -0300

    Arreglo plataformas

--- scenes/game_elements/platforms/platform_1.gd
+++ scenes/game_elements/platforms/platform_1.gd
@@ -4,15 +4,16 @@ extends StaticBody2D
 @export var size_in_cells: Vector2i = Vector2i(1, 1):
 	set(new_value):
 		size_in_cells = new_value.max(Vector2i(1, 1))
-		return
 		if not is_node_ready():
 			await ready
-
+		if not Engine.is_editor_hint():
+			return
+		owner.set_editable_instance(self, true)
+		
 		var size = 32 * Vector2(size_in_cells)
 		$Mask/Sprite2D.texture.width = size.x
 		$Mask/Sprite2D.texture.height = size.y
 		
-		$Mask.position = size / 2
 		$CollisionPolygon2D.polygon = PackedVector2Array(
 			[
 				Vector2(0, 0),
