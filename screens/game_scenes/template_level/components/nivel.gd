7ea6c115ce37672042d87aba6d11d70ffa92c97f
Facundo Javier Gelatti <javiergelatti@gmail.com>
2026-02-01 02:11:15 -0300

    Exportar tamaño de los canvas en Nivel, hacer que las cámaras sigan la posición y rotación de los canvas

--- screens/game_scenes/template_level/components/nivel.gd
+++ screens/game_scenes/template_level/components/nivel.gd
@@ -8,6 +8,24 @@ extends Node2D
 			await ready
 		%PreviewMask.mask_size = mask_size
 		%MaskSelection.mask_size = mask_size
+		
+@export var canvas_size: Vector2i = Vector2i(600, 600):
+	set(new_value):
+		canvas_size = new_value
+		if not is_node_ready():
+			await ready
+		
+		%LayerBackground.texture.width = canvas_size.x
+		%LayerBackground.texture.height = canvas_size.y
+		$Layer/StaticBody2D/Bottom.position = canvas_size
+		$Layer/StaticBody2D/Right.position = canvas_size
+		
+		%MaskBackground.texture.width = canvas_size.x
+		%MaskBackground.texture.height = canvas_size.y
+		
+		%MaskSubviewport.size = canvas_size
+		%LayerSubviewport.size = canvas_size
+
 
 func _ready() -> void:
 	%MaskSubviewport.size = %LayerBackground.texture.get_size()
--- screens/game_scenes/template_level/components/sub_viewport.gd
+++ screens/game_scenes/template_level/components/sub_viewport.gd
@@ -1,4 +1,9 @@
 extends SubViewport
 
+@export var camera: Camera2D
+@export var canvas: Node2D
+
 func _ready() -> void:
 	world_2d = get_tree().root.get_viewport().world_2d
+	camera.offset = canvas.position
+	camera.rotation = canvas.rotation
