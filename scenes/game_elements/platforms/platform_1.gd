1cb90a4823699a5c315add015b5fd0724d4194f8
Facundo Javier Gelatti <javiergelatti@gmail.com>
2026-02-01 22:19:42 -0300

    Arreglar textura de las plataformas

--- scenes/game_elements/platforms/platform_1.gd
+++ scenes/game_elements/platforms/platform_1.gd
@@ -19,6 +19,7 @@ extends CollisionObject2D
 			await ready
 		if not Engine.is_editor_hint():
 			return
+		new_value.resource_local_to_scene = true
 		sprite_2d.texture = texture
 		update_based_on_size_in_cells()
 
@@ -27,13 +28,13 @@ func update_based_on_size_in_cells():
 		owner.set_editable_instance(self, true)
 	
 	var size = 32 * Vector2(size_in_cells)
-	#if $Mask/Sprite2D.texture is GradientTexture2D:
-		#$Mask/Sprite2D.texture.width = size.x
-		#$Mask/Sprite2D.texture.height = size.y
-		#$Mask/Sprite2D.region_enabled = false
-	#elif $Mask/Sprite2D.texture is CompressedTexture2D:
-		#$Mask/Sprite2D.region_enabled = true
-		#$Mask/Sprite2D.region_rect = Rect2(0, 0, size.x, size.y)
+	if $Mask/Sprite2D.texture is GradientTexture2D:
+		$Mask/Sprite2D.texture.width = size.x
+		$Mask/Sprite2D.texture.height = size.y
+		$Mask/Sprite2D.region_enabled = false
+	elif $Mask/Sprite2D.texture is CompressedTexture2D:
+		$Mask/Sprite2D.region_enabled = true
+		$Mask/Sprite2D.region_rect = Rect2(0, 0, size.x, size.y)
 	
 	$CollisionPolygon2D.polygon = PackedVector2Array(
 		[
